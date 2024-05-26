32d0e2433192c9ded2387ee9aaaa6cf2f7d94418
PankajKumar <pk2018student@gmail.com>
2024-05-27 01:29:23 +0530

    Repository Layer Init

---
+++ ShopServerSolution/ShopServer/Repositories/Interface/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ShopServer.Repositories.Interface
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ ShopServerSolution/ShopServer/Repositories/Interface/IProductRepository .cs
@@ -0,0 +1,9 @@
+﻿using ShopServer.Model.Domain;
+
+namespace ShopServer.Repositories.Interface
+{
+    public interface IProductRepository : IBaseRepository<Product>
+    {
+        Task<IEnumerable<Product>> GetProductsByCategoryAsync(string category);
+    }
+}
